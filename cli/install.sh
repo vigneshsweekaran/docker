ec8e0c1d7c825460ccb0a6cabae866c8c96bac0f
Vignesh Sweekaran <40670015+vigneshsweekaran@users.noreply.github.com>
2022-07-12 19:19:55 +0530

    Removed sudo

--- cli/install.sh
+++ cli/install.sh
@@ -8,10 +8,10 @@ apt-get update && apt-get install -y gnupg software-properties-common curl pytho
 
 # Installing docker cli
 mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
 apt-get install docker-ce-cli
 
