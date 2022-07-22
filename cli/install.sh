3b1c437bd55d3bae619f8be662d13462914184c2
Vignesh Sweekaran <40670015+vigneshsweekaran@users.noreply.github.com>
2022-07-22 23:35:44 +0530

    Update install.sh

--- cli/install.sh
+++ cli/install.sh
@@ -17,6 +17,7 @@ apt-get install -y docker-ce-cli
 
 # Installing kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
 # Installing helm
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
