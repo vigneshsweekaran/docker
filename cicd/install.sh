b54a5a493f3af07584fb4fb599445c7c77ad1f52
Vignesh Sweekaran <40670015+vigneshsweekaran@users.noreply.github.com>
2022-08-15 21:11:29 +0530

    Update install.sh

--- cicd/install.sh
+++ cicd/install.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Updating and installing packages
-apt-get update && apt-get install -y gnupg software-properties-common curl python3 python3-pip \
+apt-get update && apt-get install -y gnupg software-properties-common git curl python3 python3-pip \
   ca-certificates ca-certificates unzip tar wget
 
 # Installing java
